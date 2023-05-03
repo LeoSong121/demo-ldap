fe350212eb90b5ebed30c1aece8329266c485928
LeoSong121 <leosong121@foxmail.com>
2023-05-03 22:34:42 +0800

    Update DemoLdapApplication.java

--- src/main/java/com/example/demo/ldap/DemoLdapApplication.java
+++ src/main/java/com/example/demo/ldap/DemoLdapApplication.java
@@ -3,11 +3,14 @@ package com.example.demo.ldap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Ldap Demo
+ */
 @SpringBootApplication
 public class DemoLdapApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoLdapApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoLdapApplication.class, args);
+    }
 
 }
